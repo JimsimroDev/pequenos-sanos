8092c03303f223bce5c40c3e400424f73b53dc6d
jimsimrodev <jhoangd.jgd@gmail.com>
2026-07-26 18:12:30 -0500

    feat: configuración inicial del proyecto

---
+++ src/main/java/uk/jimsimrodev/pequenos_sanos/PequenosSanosApplication.java
@@ -0,0 +1,13 @@
+package uk.jimsimrodev.pequenos_sanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PequenosSanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PequenosSanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/jimsimrodev/pequenos_sanos/PequenosSanosApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.jimsimrodev.pequenos_sanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PequenosSanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
